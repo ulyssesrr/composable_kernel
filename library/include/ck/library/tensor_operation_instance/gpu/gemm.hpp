5e8ee548f7bc999e4ec9b8950ff9c83d4e3d9e74
Po-Yen, Chen <PoYen.Chen@amd.com>
2023-03-24 03:38:39 +0800

    Add instance for layout=NT

--- library/include/ck/library/tensor_operation_instance/gpu/gemm.hpp
+++ library/include/ck/library/tensor_operation_instance/gpu/gemm.hpp
@@ -309,7 +309,7 @@ struct DeviceOperationInstanceFactory<
             else if constexpr(is_same_v<ALayout, Col> && is_same_v<BLayout, Row> &&
                               is_same_v<CLayout, Row>)
             {
-                // add_device_gemm_xdl_f16_f16_f16_km_kn_mn_instances(op_ptrs);
+                add_device_gemm_xdl_f16_f16_f16_km_kn_mn_instances(op_ptrs);
                 // add_device_gemm_dl_f16_f16_f16_km_kn_mn_instances(op_ptrs);
                 // add_device_gemm_xdl_c_shuffle_f16_f16_f16_km_kn_mn_instances(op_ptrs);
             }
